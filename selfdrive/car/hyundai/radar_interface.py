c99994669fbc93f09a75d28f92e5a7ce8e513ae3
Radek Chybicki <rchybicki@gmail.com>
2023-01-22 09:47:51 +0100

    commented out code for radar, tall vehicle fix

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -56,8 +56,10 @@ class RadarInterface(RadarInterfaceBase):
     self.rcp = get_radar_can_parser(CP)
 
   def update(self, can_strings):
-    if self.radar_off_can or (self.rcp is None):
-      return super().update(None)
+    # if self.radar_off_can or (self.rcp is None):
+    #   return super().update(None)
+    # if self.rcp is None:
+    #   return super().update(None)
 
     vls = self.rcp.update_strings(can_strings)
     self.updated_messages.update(vls)
