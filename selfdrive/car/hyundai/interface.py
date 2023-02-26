de55549fa72a2cdd7d650bd05e437a6f2ca43d38
Radek Chybicki <rchybicki@gmail.com>
2023-02-26 17:10:02 +0100

    accel tune

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -233,9 +233,9 @@ class CarInterface(CarInterfaceBase):
       # ret.longitudinalTuning.kpV = [0.1]
       # ret.longitudinalTuning.kiV = [0.0]
       ret.longitudinalTuning.kpBP = [0., 10., 40.]
-      ret.longitudinalTuning.kpV = [0.5, 0.025, 0.025]
+      ret.longitudinalTuning.kpV = [0.75, 0.025, 0.025]
       ret.longitudinalTuning.kiBP = [0., 10., 30., 40.]
-      ret.longitudinalTuning.kiV = [0.3, 0.05, 0.01, 0.005]
+      ret.longitudinalTuning.kiV = [0.2, 0.05, 0.01, 0.005]
       ret.longitudinalTuning.deadzoneBP = [0., 40]
       ret.longitudinalTuning.deadzoneV = [0., 0.02]
       ret.experimentalLongitudinalAvailable = candidate not in (LEGACY_SAFETY_MODE_CAR | CAMERA_SCC_CAR)
