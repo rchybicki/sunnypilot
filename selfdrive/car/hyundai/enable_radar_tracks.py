febf3202d379c6929562525d0e444c79bd38ec52
Radek Chybicki <rchybicki@gmail.com>
2023-03-01 07:56:19 +0100

    script param fix

--- selfdrive/car/hyundai/enable_radar_tracks.py
+++ selfdrive/car/hyundai/enable_radar_tracks.py
@@ -14,14 +14,14 @@ def enable_radar_tracks(CP, logcan, sendcan, debug=False):
   try:
     for i in range(40):
       try:
-        query = IsoTpParallelQuery(sendcan, logcan, 0, [rdr_fw.address], [b'\x10\x07'], [b'\x50\x07'], debug)
+        query = IsoTpParallelQuery(sendcan, logcan, 0, [rdr_fw.address], [b'\x10\x07'], [b'\x50\x07'], debug = debug)
         for addr, dat in query.get_data(0.1).items(): # pylint: disable=unused-variable
           print("ecu write data by id ...")
           new_config = b"\x00\x00\x00\x01\x00\x01"
           dataId = b'\x01\x42'
           WRITE_DAT_REQUEST = b'\x2e'
           WRITE_DAT_RESPONSE = b'\x68'
-          query = IsoTpParallelQuery(sendcan, logcan, 0, [rdr_fw.address], [WRITE_DAT_REQUEST+dataId+new_config], [WRITE_DAT_RESPONSE], debug)
+          query = IsoTpParallelQuery(sendcan, logcan, 0, [rdr_fw.address], [WRITE_DAT_REQUEST+dataId+new_config], [WRITE_DAT_RESPONSE], debug = debug)
           query.get_data(0)
           print(f"Try {i+1}")
           break
