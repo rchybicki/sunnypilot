efb53e6f443b8d3beecebcbffa82ffae66ddb946
rchybicki <rchybicki@gmail.com>
2023-03-05 12:31:36 +0100

    accel tune

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -232,10 +232,10 @@ class CarInterface(CarInterfaceBase):
       # ret.longitudinalTuning.kpV = [0.05, 0.025, 0.01]
       # ret.longitudinalTuning.kpV = [0.1]
       # ret.longitudinalTuning.kiV = [0.0]
-      ret.longitudinalTuning.kpBP = [0., 10.,  20.,   40.]
-      ret.longitudinalTuning.kpV = [0.3,  0.05, 0.01,  0.005]
-      ret.longitudinalTuning.kiBP = [0., 10.,   40.]
-      ret.longitudinalTuning.kiV = [0.08, 0.01, 0.0005]
+      ret.longitudinalTuning.kpBP = [ 0.01, 0.05,  0.2,  0.3, 10.,  20.,   40.]
+      ret.longitudinalTuning.kpV =  [ 1.,   0.025, 0.05, 0.1,  0.05, 0.01,  0.005]
+      ret.longitudinalTuning.kiV =  [ 0.1,  0.005, 0.01, 0.02, 0.01, 0.0005,0.0001]
+      ret.longitudinalTuning.kiBP = kpBP
       ret.longitudinalTuning.deadzoneBP = [0., 40]
       ret.longitudinalTuning.deadzoneV = [0., 0.02]
       ret.experimentalLongitudinalAvailable = candidate not in (LEGACY_SAFETY_MODE_CAR | CAMERA_SCC_CAR)
@@ -250,7 +250,7 @@ class CarInterface(CarInterfaceBase):
     ret.startingState = True
     ret.vEgoStarting = 0.1       # was 0.1
     ret.vEgoStopping = 0.1        # was 0.1
-    ret.startAccel = 0.25
+    ret.startAccel = 0.1
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
 
