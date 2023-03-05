cf28938e04d616e41492c81bce2b67448f4005f3
rchybicki <rchybicki@gmail.com>
2023-03-05 11:16:30 +0100

    comma fix

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -103,7 +103,7 @@ class LongControl:
 
     elif self.long_control_state == LongCtrlState.stopping:  
       output_accel = min(output_accel, 0.0)
-      stopping_accel = [-0.2, -0.1,   -0.1,   -0.25  -0.5,   -2.0 ]
+      stopping_accel = [-0.2, -0.1,   -0.1,   -0.25, -0.5,   -2.0 ]
       stopping_step =  [ 2.,   0.01,   0.025,  0.05,  0.3,    0.5  ]
       stopping_v_bp =  [ 0.01, 0.05,   0.2,    0.3,   0.5,    2.0 ]
       expected_accel = interp(CS.vEgo, stopping_v_bp, stopping_accel)
