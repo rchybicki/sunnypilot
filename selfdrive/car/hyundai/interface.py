81f874312caae6dabbe09e97830ebed5879f42a2
rchybicki <rchybicki@gmail.com>
2023-03-07 09:39:25 +0100

    stopping tune

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -261,9 +261,9 @@ class CarInterface(CarInterfaceBase):
 
     ret.stoppingControl = True
     ret.startingState = True
-    ret.vEgoStarting = 0.25       # was 0.1
-    ret.vEgoStopping = 0.1        # was 0.1
-    ret.startAccel = 0.5
+    ret.vEgoStarting = 0.5       # was 0.1
+    ret.vEgoStopping = 0.5       # was 0.1
+    ret.startAccel = 0.25
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
 
--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -109,7 +109,7 @@ class LongControl:
     elif self.long_control_state == LongCtrlState.stopping:  
       output_accel = min(output_accel, 0.0)
       stopping_accel = [-0.2, -0.1,  -0.1,  -0.2, -0.5,  -2.0  ] 
-      stopping_step =  [ 3.,   2.,    3.,    3.,   3.,    3.   ]
+      stopping_step =  [ 3.,   1.,    2.,    2.,   3.,    3.   ]
       stopping_v_bp =  [ 0.01, 0.05,  0.2,   0.3,  0.5,   2.0  ]
       expected_accel = interp(CS.vEgo, stopping_v_bp, stopping_accel)
 
