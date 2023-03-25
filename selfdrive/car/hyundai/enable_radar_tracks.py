e3cf6e061999a76ca5fda3f33a5d5b3bd1204d76
rchybicki <rchybicki@gmail.com>
2023-03-25 12:07:38 +0100

    RCH Changes

---
+++ selfdrive/car/hyundai/enable_radar_tracks.py
@@ -0,0 +1,33 @@
+
+from selfdrive.car.isotp_parallel_query import IsoTpParallelQuery
+
+
+def enable_radar_tracks(CP, logcan, sendcan, debug=False):
+  print("Try to enable radar tracks")
+  rdr_fw = None
+  for fw in CP.carFw:
+    if fw.ecu == "fwdRadar":
+      rdr_fw = fw
+      break
+  if rdr_fw is not None:
+    print(f"Found fwdRadar: {rdr_fw.fwVersion}")
+    for i in range(40):
+      try:
+        query = IsoTpParallelQuery(sendcan, logcan, 0, [rdr_fw.address], [b'\x10\x07'], [b'\x50\x07'],  debug = debug)
+        for addr, dat in query.get_data(0.1).items(): # pylint: disable=unused-variable
+          print(f"ecu write data by id try {i+1} ...")
+          new_config = b"\x00\x00\x00\x01\x00\x01"
+          dataId = b'\x01\x42'
+          WRITE_DAT_REQUEST = b'\x2e'
+          WRITE_DAT_RESPONSE = b'\x68'
+          query = IsoTpParallelQuery(sendcan, logcan, 0, [rdr_fw.address], [WRITE_DAT_REQUEST+dataId+new_config], [WRITE_DAT_RESPONSE],  debug = debug)
+          query.get_data(0)
+          break
+        print(f"Radar tracks enabled.")
+        CP.radarUnavailable = False
+        break
+      except Exception as e:
+        print(f"Failed {i}: {e}") 
+  else:
+    print("Failed to find fwdRadar")
+  print("END Try to enable radar tracks")
--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -18,18 +18,50 @@ from selfdrive.controls.lib.turn_speed_controller import TurnSpeedController
 from selfdrive.controls.lib.events import Events
 from system.swaglog import cloudlog
 
+
 LON_MPC_STEP = 0.2  # first step is 0.2s
-A_CRUISE_MIN = -1.2
-A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
-A_CRUISE_MAX_BP = [0., 10.0, 25., 40.]
+
+A_CRUISE_MAX_VAL_GAP4 = [ 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5,  0.5,  0.5,  0.4,  0.2]
+A_CRUISE_MAX_VAL_GAP3 = [ 0.9, 1.1, 1.2, 1.3, 1.3, 1.2, 1.0,  0.8,  0.6,  0.5,  0.3]
+A_CRUISE_MAX_VAL_GAP2 = A_CRUISE_MAX_VAL_GAP3 #[ 1.2, 1.3, 1.4, 1.4, 1.3, 1.2, 1.0,  0.8,  0.6,  0.5,  0.3]
+A_CRUISE_MAX_VAL_GAP1 = A_CRUISE_MAX_VAL_GAP2 #[ 1.4, 1.5, 1.6, 1.5, 1.3, 1.2, 1.0,  0.8,  0.6,  0.5,  0.3]
+             # in kph      0  3.6   7.2   18   28   39   54    72    90   108   195
+A_CRUISE_MAX_BP =       [ 0., 1.,   2.,   5.,  8.,  11., 15.,  20.,  25.,  30., 55.]
+
+# _DP_CRUISE_MAX_V =       [3.5, 3.4, 2.1, 1.6, 1.1, 0.91, 0.68, 0.44, 0.34, 0.13]
+# _DP_CRUISE_MAX_V_ECO =   [3.0, 1.7, 1.3, 0.7, 0.6, 0.44, 0.32, 0.22, 0.16, 0.0078]
+# _DP_CRUISE_MAX_V_SPORT = [3.5, 3.5, 3.4, 3.0, 2.1, 1.61, 1.1,  0.63, 0.50, 0.33]
+# _DP_CRUISE_MAX_BP =      [0.,  3,   6.,  8.,  11., 15.,  20.,  25.,  30.,  55.]
+
+#A_CRUISE_MAX_VALS = [1.4, 1.0, 0.7, 0.5] # was [1.6, 1.2, 0.8, 0.6]
+#A_CRUISE_MAX_BP = [0., 10.0, 25., 40.] # 0km/h, 36km/h, 90km/h, 144km/h
+
+CRUISE_MIN_VAL_GAP3 =       [-0.65,  -0.60,  -0.9,  -1.0,   -1.1,  -1.5 ]
+CRUISE_MIN_VAL_GAP4 = CRUISE_MIN_VAL_GAP3 #      [-0.65,  -0.60,  -0.73, -0.75,  -0.75, -0.75 ]
+CRUISE_MIN_VAL_GAP2 = CRUISE_MIN_VAL_GAP3 #      [-0.65,  -0.60,  -1.3,  -1.2,   -1.6,  -2.0  ]
+CRUISE_MIN_VAL_GAP1 = CRUISE_MIN_VAL_GAP3 #      [-0.65,  -0.60,  -1.5,  -1.7,   -2.0,  -2.5  ]
+#                              0       0.5       36     72      108   195
+CRUISE_MIN_BP =             [ 0.,     0.07,   10.,   20.,    30.,   55.  ]
+
+# _DP_CRUISE_MIN_V =       [-0.65,  -0.60,  -0.73, -0.75,  -0.75, -0.75]
+# _DP_CRUISE_MIN_V_ECO =   [-0.65,  -0.60,  -0.70, -0.70,  -0.65, -0.65]
+# _DP_CRUISE_MIN_V_SPORT = [-0.70,  -0.80,  -0.90, -0.90,  -0.80, -0.70]
+# _DP_CRUISE_MIN_BP =      [0.,     0.07,   10.,   20.,    30.,   55.]
 
 # Lookup table for turns
 _A_TOTAL_MAX_V = [1.7, 3.2]
 _A_TOTAL_MAX_BP = [20., 40.]
 
 
-def get_max_accel(v_ego):
-  return interp(v_ego, A_CRUISE_MAX_BP, A_CRUISE_MAX_VALS)
+def get_min_max_accel(v_ego, carstate):
+  if carstate.gapAdjustCruiseTr == 1:
+    return [interp(v_ego, CRUISE_MIN_BP, CRUISE_MIN_VAL_GAP1), interp(v_ego, A_CRUISE_MAX_BP, A_CRUISE_MAX_VAL_GAP1)]
+  elif carstate.gapAdjustCruiseTr == 2:
+    return [interp(v_ego, CRUISE_MIN_BP, CRUISE_MIN_VAL_GAP2), interp(v_ego, A_CRUISE_MAX_BP, A_CRUISE_MAX_VAL_GAP2)]
+  elif carstate.gapAdjustCruiseTr == 4:
+    return [interp(v_ego, CRUISE_MIN_BP, CRUISE_MIN_VAL_GAP4), interp(v_ego, A_CRUISE_MAX_BP, A_CRUISE_MAX_VAL_GAP4)]
+  else:
+    return [interp(v_ego, CRUISE_MIN_BP, CRUISE_MIN_VAL_GAP3), interp(v_ego, A_CRUISE_MAX_BP, A_CRUISE_MAX_VAL_GAP3)]
 
 
 def limit_accel_in_turns(v_ego, angle_steers, a_target, CP):
@@ -102,7 +134,7 @@ class LongitudinalPlanner:
     prev_accel_constraint = not (reset_state or sm['carState'].standstill)
 
     if self.mpc.mode == 'acc':
-      accel_limits = [A_CRUISE_MIN, get_max_accel(v_ego)]
+      accel_limits = get_min_max_accel(v_ego, sm['carState'])
       accel_limits_turns = limit_accel_in_turns(v_ego, sm['carState'].steeringAngleDeg, accel_limits, self.CP)
     else:
       accel_limits = [MIN_ACCEL, MAX_ACCEL]
@@ -119,8 +151,6 @@ class LongitudinalPlanner:
     if len(sm['modelV2'].temporalPose.trans):
       self.v_model_error = sm['modelV2'].temporalPose.trans[0] - v_ego
 
-    accel_limits = [A_CRUISE_MIN, get_max_accel(v_ego)]
-    accel_limits_turns = limit_accel_in_turns(v_ego, sm['carState'].steeringAngleDeg, accel_limits, self.CP)
     if force_slow_decel:
       v_cruise = 0.0
 
@@ -173,7 +203,7 @@ class LongitudinalPlanner:
     longitudinalPlan.solverExecutionTime = self.mpc.solve_time
 
     longitudinalPlan.e2eX = self.mpc.e2e_x.tolist()
-    longitudinalPlan.desiredTF = self.mpc.desired_TF
+    longitudinalPlan.desiredTF = float(self.mpc.desired_TF)
 
     longitudinalPlan.visionTurnControllerState = self.vision_turn_controller.state
     longitudinalPlan.visionTurnSpeed = float(self.vision_turn_controller.v_turn)
