128d40e20cb348aca57657b41914f1ab28f5b410
RCH <rchybicki@gmail.com>
2023-05-31 23:11:11 +0200

    disable lane change

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -53,7 +53,7 @@ class DesireHelper:
                              0.5 if lane_change_set_timer == 2 else 1.0 if lane_change_set_timer == 3 else \
                              1.5 if lane_change_set_timer == 4 else 2.0
     v_ego = carstate.vEgo
-    one_blinker = carstate.leftBlinker != carstate.rightBlinker
+    one_blinker = False #carstate.leftBlinker != carstate.rightBlinker
     below_lane_change_speed = v_ego < LANE_CHANGE_SPEED_MIN
 
     # Lane detection by FrogAi
