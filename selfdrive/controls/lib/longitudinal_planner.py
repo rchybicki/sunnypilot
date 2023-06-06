5f45f5fc4077b0df9e79de305b2ff2bc7a61c645
RCH <rchybicki@gmail.com>
2023-06-06 23:19:56 +0200

    upstream longitudinal_planner update, speed overrides

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -155,7 +155,7 @@ class LongitudinalPlanner:
     reset_state = long_control_off if self.CP.openpilotLongitudinalControl else not sm['controlsState'].enabled
 
     # No change cost when user is controlling the speed, or when standstill
-    prev_accel_constraint = not sm['carState'].standstill
+    prev_accel_constraint = not (reset_state or sm['carState'].standstill)
 
     # Update VehicleModel
     lp = sm['liveParameters']
