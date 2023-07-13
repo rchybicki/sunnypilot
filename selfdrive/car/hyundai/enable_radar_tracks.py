1affa1aaa3029a5146d3a3342ac57e7ded3d9aa6
RCH <rchybicki@gmail.com>
2023-07-13 12:30:37 +0200

    restore new radar tracks script

--- selfdrive/car/hyundai/enable_radar_tracks.py
+++ selfdrive/car/hyundai/enable_radar_tracks.py
@@ -1,33 +1,70 @@
+import time
+from typing import NamedTuple
 
 from selfdrive.car.isotp_parallel_query import IsoTpParallelQuery
+from system.swaglog import cloudlog
+
+DIAG_REQUEST = b'\x10\x07'
+DIAG_RESPONSE = b'\x50\x07'
+
+WRITE_DATA_REQUEST = b'\x2e'
+WRITE_DATA_RESPONSE = b'\x68'
 
 
-def enable_radar_tracks(CP, logcan, sendcan, debug=False):
-  print("Try to enable radar tracks")
-  rdr_fw = None
-  for fw in CP.carFw:
-    if fw.ecu == "fwdRadar":
-      rdr_fw = fw
-      break
-  if rdr_fw is not None:
-    print(f"Found fwdRadar: {rdr_fw.fwVersion}")
-    for i in range(40):
-      try:
-        query = IsoTpParallelQuery(sendcan, logcan, 0, [rdr_fw.address], [b'\x10\x07'], [b'\x50\x07'],  debug = debug)
-        for addr, dat in query.get_data(0.1).items(): # pylint: disable=unused-variable
-          print(f"ecu write data by id try {i+1} ...")
-          new_config = b"\x00\x00\x00\x01\x00\x01"
-          dataId = b'\x01\x42'
-          WRITE_DAT_REQUEST = b'\x2e'
-          WRITE_DAT_RESPONSE = b'\x68'
-          query = IsoTpParallelQuery(sendcan, logcan, 0, [rdr_fw.address], [WRITE_DAT_REQUEST+dataId+new_config], [WRITE_DAT_RESPONSE],  debug = debug)
-          query.get_data(0)
-          break
-        print(f"Radar tracks enabled.")
-        CP.radarUnavailable = False
-        break
-      except Exception as e:
-        print(f"Failed {i}: {e}") 
+class ConfigValues(NamedTuple):
+  tracks_enabled: bytes
+
+
+# If your radar supports changing data identifier 0x0142 as well make a PR to
+# this file to add your firmware version. Make sure to post a drive as proof!
+SUPPORTED_FW_VERSIONS = {
+  # 2022 Santa Fe
+  b'\xf1\x00TM__ SCC FHCUP      1.00 1.00 99110-S1500         ': ConfigValues(
+    tracks_enabled=b"\x00\x00\x00\x01\x00\x01"
+  ),
+  # 2022 Santa Fe HEV
+  b'\xf1\x00TMhe SCC FHCUP      1.00 1.00 99110-CL500         ': ConfigValues(
+    tracks_enabled=b"\x00\x00\x00\x01\x00\x01"
+  ),
+}
+
+
+def _enable_radar_tracks(logcan, sendcan, fw_version, bus=0, addr=0x7d0, config_data_id=b'\x01\x42', timeout=0.1, retry=10, debug=False):
+  cloudlog.warning("radar_tracks: enabling...")
+
+  for i in range(retry):
+    try:
+      query = IsoTpParallelQuery(sendcan, logcan, bus, [addr], [DIAG_REQUEST], [DIAG_RESPONSE], debug=debug)
+
+      for _, _ in query.get_data(timeout).items():
+        cloudlog.warning("radar_tracks: reconfigure radar to output radar points ...")
+        config_value = SUPPORTED_FW_VERSIONS[fw_version]
+        new_config = config_value.tracks_enabled
+
+        query = IsoTpParallelQuery(sendcan, logcan, bus, [addr],
+                                   [WRITE_DATA_REQUEST + config_data_id + new_config],
+                                   [WRITE_DATA_RESPONSE], debug=debug)
+        query.get_data(0)
+
+        cloudlog.warning("radar_tracks: successfully enabled")
+        return True
+
+    except Exception as e:
+      cloudlog.exception(f"radar_tracks exception: {e}")
+
+    cloudlog.error(f"radar_tracks retry ({i + 1}) ...")
+  cloudlog.error(f"radar_tracks: failed")
+  return False
+
+
+
+def enable_radar_tracks(CP, logcan, sendcan):
+  cloudlog.warning("radar_tracks: Try to enable radar tracks")
+
+  has_radar = next((fw for fw in CP.carFw if fw.ecu == "fwdRadar"), None)
+  fw_version = has_radar.fwVersion if has_radar is not None else None
+
+  if fw_version is not None and fw_version in SUPPORTED_FW_VERSIONS.keys():
+    _enable_radar_tracks(logcan, sendcan, fw_version)
   else:
-    print("Failed to find fwdRadar")
-  print("END Try to enable radar tracks")
+    cloudlog.error("radar_tracks: radar not supported! Skipping ...")
