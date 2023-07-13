90143e4fe1ea06e4c07cade83d25a7e1ed39edd4
RCH <rchybicki@gmail.com>
2023-07-13 11:49:49 +0200

    cloudlog logging

--- selfdrive/car/hyundai/radar_interface.py
+++ selfdrive/car/hyundai/radar_interface.py
@@ -5,6 +5,7 @@ from cereal import car
 from opendbc.can.parser import CANParser
 from selfdrive.car.interfaces import RadarInterfaceBase
 from selfdrive.car.hyundai.values import DBC, HyundaiFlagsSP
+from system.swaglog import cloudlog
 
 RADAR_START_ADDR = 0x500
 RADAR_MSG_COUNT = 32
@@ -56,22 +57,22 @@ class RadarInterface(RadarInterfaceBase):
     self.rcp = get_radar_can_parser(CP)
 
   def update(self, can_strings):
-    print("radar_interface start")
+    cloudlog.warning("radar_interface start")
     if self.radar_off_can or (self.rcp is None):
-      print("radar_interface first check")
+      cloudlog.warning("radar_interface first check")
       return super().update(None)
 
     vls = self.rcp.update_strings(can_strings)
     self.updated_messages.update(vls)
 
     if self.trigger_msg not in self.updated_messages:
-      print("radar_interface second check")
+      cloudlog.warning("radar_interface second check")
       return super().update(None)
 
     rr = self._update(self.updated_messages)
     self.updated_messages.clear()
     
-    print("radar_interface all good")
+    cloudlog.warning("radar_interface all good")
     return rr
 
   def _update(self, updated_messages):
